9776bbb30b0f91c781946bcef52785d0c2cd49aa
yatharth <yatharth.agarwal10@gmail.com>
2025-07-17 18:06:05 +0530

    added all files

---
+++ firebaseConfig.js
@@ -0,0 +1,21 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA0dVVXBstVtrxgSIoqIa9qw3A7BrlZ06A",
+//   authDomain: "real-class-21.firebaseapp.com",
+//   projectId: "real-class-21",
+//   storageBucket: "real-class-21.firebasestorage.app",
+//   messagingSenderId: "967868167197",
+//   appId: "1:967868167197:web:c3dc25a8d2a8730ae3c2f4",
+//   measurementId: "G-07L32PR8G4"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
